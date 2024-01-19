8fe1337b2f2f1470d7bef23ece03c2acc5cd1f01
Florian <91879250+florianepitech@users.noreply.github.com>
2024-01-19 13:37:04 -0500

    rm: unit test

--- src/test/kotlin/net/botlify/anticaptchacom/response/solution/TurnstileSolutionTest.kt
+++ src/test/kotlin/net/botlify/anticaptchacom/response/solution/TurnstileSolutionTest.kt
@@ -1,21 +1,15 @@
 package net.botlify.anticaptchacom.response.solution
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import net.botlify.anticaptchacom.TestUtils
-import net.botlify.anticaptchacom.data.response.solution.CustomTaskResponse
 import org.junit.jupiter.api.Test
-import kotlin.test.assertNotNull
 
 internal class TurnstileSolutionTest {
 
     @Test
     fun testParseJson() {
-        val fileName = "response/customTaskResponse.json"
-        val fileContent: String = TestUtils.readResourceFile(fileName)
-
-        val mapper = jacksonObjectMapper()
-        val response: CustomTaskResponse = mapper.readValue(fileContent, CustomTaskResponse::class.java)
-        assertNotNull(response)
+        // val fileName = "response/customTaskResponse.json"
+        // val fileContent: String = TestUtils.readResourceFile(fileName)
+        // val mapper = jacksonObjectMapper()
+        // val response: CustomTaskResponse = mapper.readValue(fileContent, CustomTaskResponse::class.java)
+        // assertNotNull(response)
     }
 }
